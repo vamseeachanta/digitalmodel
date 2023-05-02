d23b4060a711b1f5efce2b3cbfaa775415f3be60
Vamsee Achanta <achantav@gmail.com>
2023-05-01 22:50:04 -0500

    wip

--- src/digitalmodel/custom/rao_analysis.py
+++ src/digitalmodel/custom/rao_analysis.py
@@ -11,12 +11,32 @@ class RAOAnalysis:
 
     def read_orcaflex_raos(self, cfg=None):
         self.cfg = cfg
+        vessel_data_all_files = {}
         for file in cfg['Files']:
             if os.path.isfile(file['Name']):
-                self.read_rao_file(file['Name'])
+                vessel_data = self.read_vessel_data_from_file(file['Name'])
+                vessel_data_all_files.update({file['Label']: vessel_data})
 
-    def read_rao_file(self, file_name):
+        self.vessel_data_all_files = vessel_data_all_files
+
+    def read_vessel_data_from_file(self, file_name):
         print('Reading file: {}'.format(file_name))
         yml_data = ymlInput(file_name, updateYml=None)
-        rao_data = yml_data['VesselTypes'][0]
-        print(rao_data)
+        vessel_data = yml_data['VesselTypes']
+        return vessel_data
+
+    def plot_amplitudes(self):
+        vessel_data_all_files = self.vessel_data_all_files
+
+        for vessel_data in vessel_data_all_files:
+            keys = list(vessel_data.keys())
+            for key in keys:
+                if key == 'RAOs':
+                    vessel_data[key]['Amplitude'].plot()
+
+        self.vessel_data_all_files['Vessel1']['RAOs']['Amplitude'].plot()
+
+
+# check amplitudes,
+# check phases,
+# check locations
