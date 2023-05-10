41d1d65bfd6f0a641c4842593828c109c39d1de0
Vamsee Achanta <achantav@gmail.com>
2023-05-10 11:22:44 -0500

    wip

--- src/digitalmodel/tests/test_catenary_riser.py
+++ src/digitalmodel/tests/test_catenary_riser.py
@@ -1,21 +1,17 @@
 import os
 import sys
 
-from digitalmodel.common.yml_utilities import ymlInput
-from digitalmodel.common.update_deep import AttributeDict
-from digitalmodel.common.ApplicationManager import ConfigureApplicationInputs
-from digitalmodel.catenary_riser import catenary_riser
+from digitalmodel.engine import engine
 
+# Run by file
 ymlfile = 'test_data/catenary_riser.yml'
 if not os.path.isfile(ymlfile):
     ymlfile = os.path.join(os.path.dirname(__file__), ymlfile)
     print(os.path.isfile(ymlfile))
+    engine(ymlfile)
 
-cfg = ymlInput(ymlfile, updateYml=None)
-cfg = AttributeDict(cfg)
-
-basename = 'catenary_riser'
-application_manager = ConfigureApplicationInputs(basename)
-application_manager.configure(run_dict=None)
-
-cfg_base = catenary_riser(application_manager.cfg)
+# Run by args
+ymlfile = 'test_data/catenary_riser.yml'
+if len(sys.argv) <= 1:
+    sys.argv.append(ymlfile)
+    engine()
