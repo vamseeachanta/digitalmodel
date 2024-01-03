d241c98abc585ad84d94b9ff203e9ebe61aa02fb
Vamsee Achanta <achantav@gmail.com>
2024-01-03 06:33:36 -0600

    wip

--- src/digitalmodel/common/ship_fatigue_analysis.py
+++ src/digitalmodel/common/ship_fatigue_analysis.py
@@ -4,7 +4,7 @@ import logging
 import pandas as pd
 import numpy as np
 
-from assetutilities.common.data import FromString
+from assetutilities.common.utilities import add_cwd_to_filename
 from assetutilities.common.data import ReadData
 from assetutilities.common.data import SaveData
 from assetutilities.common.visualizations import Visualization
@@ -16,4 +16,24 @@ class ShipFatigueAnalysis():
         pass
 
     def router(self, cfg):
-        pass
+        if cfg['inputs']['files']['lcf']['file_type'] == 'seasam_xtract':
+            self.get_fatigue_states(cfg)
+
+    def get_fatigue_states(self, cfg):
+        fatigue_states = cfg['inputs']['files']['lcf']['fatigue_states']
+
+        for fatigue_state in fatigue_states:
+            if not os.path.isdir(fatigue_state):
+                cwd = os.getcwd()
+                fatigue_states[0] = add_cwd_to_filename(fatigue_state, cwd)
+                if not os.path.isdir(fatigue_state):
+                    raise ValueError(f'Invalid directory: {fatigue_states[0]}')
+
+        seasam_xtract_file = cfg['inputs']['files']['lcf']['file_name']
+        seasam_xtract_file = os.path.join(os.path.dirname(__file__), seasam_xtract_file)
+        seasam_xtract_file = ReadData(seasam_xtract_file)
+        seasam_xtract_file = seasam_xtract_file.read_seasam_xtract()
+
+    def sesam_xtract_from_file(self, seasam_xtract_file):
+        seasam_xtract_file = ReadData(seasam_xtract_file)
+        seasam_xtract_file = seasam_xtract_file.read_seasam_xtract()
