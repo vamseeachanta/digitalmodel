bb6c927a321fe5b901ee5453e33740f4cc61f317
Vamsee Achanta <achantav@gmail.com>
2024-01-04 21:32:50 -0600

    wip

--- src/digitalmodel/common/ship_fatigue_analysis.py
+++ src/digitalmodel/common/ship_fatigue_analysis.py
@@ -29,11 +29,29 @@ class ShipFatigueAnalysis():
                 if not os.path.isdir(fatigue_state):
                     raise ValueError(f'Invalid directory: {fatigue_states[0]}')
 
+        state_0_files = self.get_files_in_directory(fatigue_states[0])
+        state_1_files = self.get_files_in_directory(fatigue_states[1])
+
+        fatigue_state_pairs = self.get_fatigue_state_pairs(state_0_files, state_1_files)
+                    
         seasam_xtract_file = cfg['inputs']['files']['lcf']['file_name']
         seasam_xtract_file = os.path.join(os.path.dirname(__file__), seasam_xtract_file)
         seasam_xtract_file = ReadData(seasam_xtract_file)
         seasam_xtract_file = seasam_xtract_file.read_seasam_xtract()
 
-    def sesam_xtract_from_file(self, seasam_xtract_file):
-        seasam_xtract_file = ReadData(seasam_xtract_file)
-        seasam_xtract_file = seasam_xtract_file.read_seasam_xtract()
+
+    def get_fatigue_state_pairs(self, state_0_files, state_1_files):
+        fatigue_state_pairs = []
+        for basename_0 in state_0_files['basenames']:
+            basename_index_1 in state_1_files['basenames']:
+                if file_0 == file_1:
+                    fatigue_state_pairs.append([file_0, file_1])
+        return fatigue_state_pairs
+
+    def get_files_in_directory(self, directory):
+        from assetutilities.engine import engine
+        cfg_file_management = {'basename': 'file_management', 'files': {'files_in_current_directory': {'flag': False, 'directory': directory}, 'extension': 'txt'}}
+        cfg_result = engine(inputfile=None, cfg=cfg_file_management)
+        files = cfg_result['file_management']
+
+        return files
