c49cd24177bf3837f8aa9edebbea04ee21fe4b50
Vamsee Achanta <achantav@gmail.com>
2024-01-09 12:11:39 -0600

    wip

--- src/digitalmodel/tests/no_license/test_ship_design_seasam_xtract.py
+++ src/digitalmodel/tests/no_license/test_ship_design_seasam_xtract.py
@@ -23,7 +23,7 @@ def get_valid_pytest_output_file(pytest_output_file):
 
 
 def test_ship_design():
-    input_file = '../test_data/ship_design/ship_design_fatigue.yml'
+    input_file = '../test_data/ship_design/ship_design_seasam_xtract.yml'
     pytest_output_file = None
     # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
     # expected_result = ymlInput(pytest_output_file, updateYml=None)
