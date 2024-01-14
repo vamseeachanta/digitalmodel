f4a2b8252b995589dcb85951d5e794f76c084c05
Vamsee Achanta <achantav@gmail.com>
2024-01-14 13:24:09 -0600

    bump version 0.0.2 -> 0.0.3

--- src/digitalmodel/__init__.py
+++ src/digitalmodel/__init__.py
@@ -4,4 +4,4 @@ See https://github.com/vamseeachanta/digitalmodel/ for more information.
 """
 
 # Version of package
-__version__ = "0.0.2"
+__version__ = "0.0.3"
--- src/digitalmodel/__main__.py
+++ src/digitalmodel/__main__.py
@@ -17,7 +17,7 @@ More information is available at:
 Version:
 --------
 
-- digitalmodel v0.0.2
+- digitalmodel v0.0.3
 """
 
 from digitalmodel.engine import engine
