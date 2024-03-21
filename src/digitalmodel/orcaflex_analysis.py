22aca21728c949bbd6fa9c1eb2264ad8390cfb87
Vamsee Achanta <achantav@gmail.com>
2024-03-20 23:31:01 -0500

    umbilical_analysis

--- src/digitalmodel/orcaflex_analysis.py
+++ src/digitalmodel/orcaflex_analysis.py
@@ -1,7 +1,9 @@
 from digitalmodel.custom.orcaflex_analysis_components import OrcaFlexAnalysis
 from digitalmodel.common.orcaflex_iterative_runs import OrcaflexIterativeRuns
 from digitalmodel.custom.orcaflex_post_process import orcaflex_post_process
+from digitalmodel.custom.orcaflex_utilities import OrcaflexUtilities
 
+ou = OrcaflexUtilities()
 oir = OrcaflexIterativeRuns()
 
 
@@ -12,7 +14,11 @@ def orcaflex_analysis(cfg):
     static_flag = cfg['orcaflex']['analysis']['static']
     simulation_flag = cfg['orcaflex']['analysis']['simulation']
     iterate_flag = cfg['orcaflex']['analysis']['iterate']['flag']
-    
+
+    if static_flag or simulation_flag or iterate_flag:
+        orcaflex_license_flag = ou.is_orcaflex_available()
+        assert (orcaflex_license_flag)
+
     if static_flag or simulation_flag:
         orcaFlex_analysis.perform_simulations()
 
