8e9019872c64ba9d2eae28d1b2a139ac33f00210
Vamsee Achanta <achantav@gmail.com>
2024-03-21 23:55:28 -0500

    umbilical analysis

--- src/digitalmodel/custom/umbilical_analysis_components.py
+++ src/digitalmodel/custom/umbilical_analysis_components.py
@@ -1,3 +1,10 @@
+import math
+import pandas as pd
+from assetutilities.common.update_deep import update_deep_dictionary
+
+from assetutilities.common.yml_utilities import ymlInput
+from assetutilities.common.data import SaveData
+save_data = SaveData()
 class UmbilicalAnalysis():
 
     def __init__(self):
@@ -5,6 +12,8 @@ class UmbilicalAnalysis():
 
     def perform_analysis(self, cfg):
         if cfg['installation_phases']:
+            # TODO program assumes host at 0 deg and No Y coordinate for installation vessel reference point. 
+            # Program this feature for future analysis
             self.installation_phases(cfg)
         # if cfg['default']['analysis']['end'] == 'first':
         #     self.first_end_analysis()
@@ -36,10 +45,69 @@ class UmbilicalAnalysis():
         return cfg
 
     def installation_step(self, cfg, step):
+        installation_step_dict  = {}
+
+        installation_step_dict = update_deep_dictionary(installation_step_dict, {"includefile": step['includefile']})
         for target_settings in step['target']:
-            target_value = self.get_target_result(cfg, target_settings)
+            target_dict = self.get_target_result(cfg, target_settings)
+            installation_step_dict = update_deep_dictionary(installation_step_dict, target_dict)
 
         # Write step file
+        filename_path = step['name']
+        save_data.saveDataYaml(installation_step_dict, filename_path, default_flow_style=False)
 
     def get_target_result(self, cfg, target_settings):
-        pass
+        if target_settings['type'] == 'line_length':
+            return self.get_target_line_length(cfg, target_settings)
+        elif target_settings['type'] == 'reference_distance':
+            return self.get_target_reference_distance(cfg, target_settings)
+        else:
+            raise NotImplementedError("Target type not implemented.")
+        
+    def get_target_line_length(self, cfg, target_settings):
+        reference_file = target_settings['reference_file']
+        reference_file_yml = ymlInput(reference_file)
+        Lines = target_settings['Lines']
+        reference_line_length = 0
+        for line in Lines:
+            columns = ['LineType', 'Length', 'TargetSegmentLength']
+            reference_file_dict = reference_file_yml['Lines'][line]['LineType, Length, TargetSegmentLength']
+            df = pd.DataFrame(reference_file_dict, columns=columns)
+            LineType = target_settings['LineType']
+            df_filter = df[df['LineType'].isin(LineType)]
+            reference_line_length = reference_line_length + df_filter['Length'].sum()
+
+        keychain_target = target_settings['keychain_target']
+        s = keychain_target[2]
+        indx = int(s[s.find("[")+1:s.find("]")])
+        current_line_length = reference_file_yml['Lines'][keychain_target[1]]['LineType, Length, TargetSegmentLength'][indx-1][1]
+        length_correction = target_settings['value'] - reference_line_length
+
+        dict  = {}
+        if length_correction != 0:
+            target_length = round(float(current_line_length + length_correction), 1)
+            dict.update({'Lines': {keychain_target[1]: {keychain_target[2]: target_length }}})
+
+        return dict
+    
+    def get_target_reference_distance(self, cfg, target_settings):
+        lay_direction = cfg['installation']['lay_direction']
+        host_reference_location = cfg['installation']['host']['reference_location']
+        installation_vessel_reference_location = cfg['installation']['installation_vessel']['reference_location']
+        reference_distance = target_settings['value']
+
+        keychain_target_vessel = target_settings['keychain_target_vessel']
+
+        initial_heading = target_settings['initial_heading']
+        initial_x = host_reference_location[0] + reference_distance*math.cos(math.radians(lay_direction)) - installation_vessel_reference_location[0]*math.cos(math.radians(initial_heading))
+        initial_y = host_reference_location[1] + reference_distance*math.sin(math.radians(lay_direction)) - installation_vessel_reference_location[0]*math.sin(math.radians(initial_heading))
+
+        dict  = {}
+        dict.update({keychain_target_vessel[0]: {keychain_target_vessel[1]: {'InitialX': round(initial_x,1), 'InitialY': round(initial_y,1), 'InitialHeading': round(initial_heading,1)}}})
+
+        keychain_target_line = target_settings['keychain_target_line']
+        endbx = initial_x  + installation_vessel_reference_location[0]*math.cos(math.radians(initial_heading))
+        endby = initial_y  + installation_vessel_reference_location[0]*math.sin(math.radians(initial_heading))
+        dict.update({"Lines": {keychain_target_line[1]: {'EndBX': round(endbx,1), 'EndBY': round(endby,1)}}})
+
+        return dict
