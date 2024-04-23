f44bb3f21fe3241eabbf949f1f2e7a4f8eb3deb8
Vamsee Achanta <achantav@gmail.com>
2024-04-23 13:40:08 -0500

    lis file method

--- src/digitalmodel/custom/aqwa_lis_files.py
+++ src/digitalmodel/custom/aqwa_lis_files.py
@@ -3,14 +3,15 @@ import math
 import logging
 import pandas as pd
 
-from assetutilities.common.utilities import is_file_valid_func
-from assetutilities.common.update_deep import update_deep_dictionary
+
 from assetutilities.common.data import ReadData
+from assetutilities.common.data import SaveData
 
 from digitalmodel.custom.aqwa_utilities import AqwaUtilities
 
 au = AqwaUtilities()
 rd = ReadData()
+save_data = SaveData()
 
 class AqwaLISFiles:
 
@@ -19,13 +20,14 @@ class AqwaLISFiles:
 
     def router(self, cfg):
         au.file_management(cfg)
-        if cfg['result']['moorings']['flag']:
-            cfg_lis = cfg['result']['moorings']
-            df = self.get_data(cfg, cfg_lis)
+        for result_item in cfg['result']:
+            df = self.get_data(cfg, result_item)
+            self.injectSummaryToExcel(df, result_item, cfg)
 
     def get_data(self, cfg, cfg_lis):
         input_files = cfg['file_management']['input_files']['LIS']
         
+        df_master = pd.DataFrame()
         for input_file in input_files:
             cfg_file = {'io': input_file, 'basename': os.path.basename(input_file)}
             cfg_keyword_lines = {
@@ -49,10 +51,8 @@ class AqwaLISFiles:
             }
 
             data = rd.from_ascii_file_get_lines_as_string_arrays(cfg_data_format)
-            df_master = pd.DataFrame()
             df_file = self.get_data_as_dataframe(data, cfg_lis, cfg_file)
-            df_master = pd.concat([df_master, df_file], axis=1)
-            
+            df_master = pd.concat([df_master, df_file], axis=0)
 
         return df_master
 
@@ -69,3 +69,18 @@ class AqwaLISFiles:
         df = pd.DataFrame(columns=data_columns)
         df.loc[len(df)] = data
         return df
+
+
+    def injectSummaryToExcel(self, df, result_item, cfg):
+        if 'inject_into' in result_item and result_item[
+                'inject_into']['flag']:
+            inject_into_file = result_item['inject_into']['filename']
+            file_name = os.path.join(cfg['Analysis']['analysis_root_folder'],
+                        inject_into_file)
+            if not os.path.isfile(file_name):
+                raise Exception(f"Inject Into File {file_name} not found for writing summary data")
+
+            sheetname = result_item['inject_into']['sheetname']
+
+            cfg_save_to_existing_workbook = {'template_file_name': file_name, 'sheetname': sheetname, 'saved_file_name': file_name, 'if_sheet_exists': 'replace', 'df': df}
+            save_data.df_to_sheet_in_existing_workbook(cfg_save_to_existing_workbook)
--- src/digitalmodel/tests/test_data/aqwa/test_aqwa_result.py
+++ src/digitalmodel/tests/test_data/aqwa/test_aqwa_result.py
@@ -7,19 +7,15 @@ from digitalmodel.custom.orcaflex_utilities import OrcaflexUtilities
 ou = OrcaflexUtilities()
 
 
-def test_orcaflex_license():
-    orcaflex_license_flag = ou.is_orcaflex_available()
-    assert (orcaflex_license_flag)
 
-
-def run_orcaflex_post_process(input_file, expected_result={}):
+def run_process(input_file, expected_result={}):
     if input_file is not None and not os.path.isfile(input_file):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
     cfg = engine(input_file)
     assert (True)
 
 
-def test_orcaflex_post_process():
+def test_run_process():
     input_file = 'aqwa.yml'
     pytest_output_file = None
     # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
@@ -28,7 +24,7 @@ def test_orcaflex_post_process():
     if len(sys.argv) > 1:
         sys.argv.pop()
 
-    run_orcaflex_post_process(input_file, expected_result={})
+    run_process(input_file, expected_result={})
 
 
-test_orcaflex_post_process()
+test_run_process()
