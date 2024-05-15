e7bfb60b0bb965043c043875cdd7211f73e5c15d
Vamsee Achanta <achantav@gmail.com>
2024-05-15 11:24:20 -0500

    wip

--- src/digitalmodel/common/cathodic_protection.py
+++ src/digitalmodel/common/cathodic_protection.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+from scipy.interpolate import interp1d
 
 class CathodicProtection():
 
@@ -18,8 +21,41 @@ class CathodicProtection():
         """
         This method is used to calculate the cathodic protection for ABS gn ships 2018
         """
+
+        breakdown_factor = self.assess_coating_breakdown(cfg)
         anode_current_capacity = self.get_anode_current_capacity(cfg)
         my_value = cfg.inputs['design_data']['seawater_max_temperature'] * cfg.inputs['design_data']['design_life']
+
+    def assess_coating_breakdown(self, cfg):
+        """
+
+        """
+        design_life = cfg['inputs']['design_life']
+        structure_cfg = cfg['inputs']['structure']
+        coated = structure_cfg['area']['value']*structure_cfg['area']['coat_percentage']/100
+        uncoated = structure_cfg['area']['value']*(1-structure_cfg['area']['coat_percentage']/100)
+        area = {'coated': coated, 'uncoated': uncoated}
+
+        coating_breakdown_cfg = structure_cfg['area']['coating_breakdown']
+
+        #TODO - implement yearly breakdown calculations using lookup from inputs
+        # for idx in range(0, math.ceil(10.3)):
+        #     year = idx +1
+        #     year_breakdown = 3
+        breakdown_percentage = coating_breakdown_cfg['percentage']
+        breakdown_factor = [1 + percentage_val/100 for percentage_val in breakdown_percentage]
+
+        cummulative_breakdown_factor = []
+        value = 1
+        for idx in range(0, len(breakdown_factor)):
+            value = value*breakdown_factor[idx]
+            cummulative_breakdown_factor.append(value)
+
+        fcm = np.mean(cummulative_breakdown_factor)
+        fcf = cummulative_breakdown_factor[-1]
+        
+        breakdown_factor = {'fcm': fcm, 'fcf': fcf}
+        return breakdown_factor
 
     def get_anode_current_capacity(self, cfg):
         """
