0c9dc1e8d0466cf652084c1cb93b7e5bba1650a0
Vamsee Achanta <achantav@gmail.com>
2024-05-17 11:20:35 -0500

    logic flow

--- src/digitalmodel/common/cathodic_protection.py
+++ src/digitalmodel/common/cathodic_protection.py
@@ -21,10 +21,12 @@ class CathodicProtection():
         """
         This method is used to calculate the cathodic protection for ABS gn ships 2018
         """
-
-        current_demand = self.current_demand_structure(cfg)
-        current_density = self.get_design_current_density(cfg)
+        structure_area = self.get_structure_area(cfg)
         breakdown_factor = self.assess_coating_breakdown(cfg)
+        current_density = self.get_design_current_density(cfg)
+
+        current_demand = self.get_current_demand(cfg, structure_area, current_density)
+
         anode_current_capacity = self.get_anode_current_capacity(cfg)
         my_value = cfg.inputs['design_data']['seawater_max_temperature'] * cfg.inputs['design_data']['design_life']
 
@@ -32,13 +34,7 @@ class CathodicProtection():
         """
 
         """
-        design_life = cfg['inputs']['design_life']
-        structure_cfg = cfg['inputs']['structure']
-        coated = structure_cfg['area']['value']*structure_cfg['area']['coat_percentage']/100
-        uncoated = structure_cfg['area']['value']*(1-structure_cfg['area']['coat_percentage']/100)
-        area = {'coated': coated, 'uncoated': uncoated}
-
-        coating_breakdown_cfg = structure_cfg['area']['coating_breakdown']
+        coating_breakdown_cfg = cfg['inputs']['structure']['area']['coating_breakdown']
 
         #TODO - implement yearly breakdown calculations using lookup from inputs
         # for idx in range(0, math.ceil(10.3)):
@@ -59,13 +55,24 @@ class CathodicProtection():
         breakdown_factor = {'fcm': fcm, 'fcf': fcf}
         return breakdown_factor
 
-    
+    def get_structure_area(self, cfg):
+        """
+
+        """
+        structure_cfg = cfg['inputs']['structure']
+        coated = structure_cfg['area']['value']*structure_cfg['area']['coat_percentage']/100
+        uncoated = structure_cfg['area']['value']*(1-structure_cfg['area']['coat_percentage']/100)
+        structure_area = {'coated': coated, 'uncoated': uncoated}
+
+        return structure_area
+        
     def get_design_current_density(self, cfg):
         """
         
         """
-        initial_coated_value = cfg['current_density']['initial']['coated']
-        initial_uncoated_value = cfg['current_density']['initial']['uncoated']
+        current_density_cfg = cfg['inputs']['current_density']
+        initial_coated_value = current_density_cfg['initial']['coated']
+        initial_uncoated_value = current_density_cfg['initial']['uncoated']
 
         final_value = self.assess_coating_breakdown(cfg)['fcf']
 
@@ -73,23 +80,39 @@ class CathodicProtection():
         disbonding_final_current_density = initial_uncoated_value * (final_value - 1)
 
         deterioration_mean = np.mean([initial_coated_value, deterioration_final_current_density])
-        disbonding_mean = np.mean([initial_coated_value, disbonding_final_current_density])
+        disbondment_mean = np.mean([initial_coated_value, disbonding_final_current_density])
 
-        current_density = {'deterioration_mean': deterioration_mean,'disbonding_mean': disbonding_mean}
-        return current_density
-
-    def current_demand_structure(self,cfg):
+        design_current_density = {'initial': {'coated': initial_coated_value, 'uncoated': initial_uncoated_value},
+            'mean': {'deterioration': {'coated': initial_coated_value, 'uncoated': initial_uncoated_value}, 
+                     'disbondment': {'coated': initial_coated_value, 'uncoated': initial_uncoated_value},},
+            'final': {'deterioration': {'coated': initial_coated_value, 'uncoated': initial_uncoated_value},
+                      'disbondment': {'coated': initial_coated_value, 'uncoated': initial_uncoated_value},}  
+            }
         
-        value = self.get_design_current_density(cfg)['initial_coated_value']
-        value_2 = self.assess_coating_breakdown(cfg)['structure_cfg']['area']['value']
-        value_3 = self.get_design_current_density(cfg)['deterioration_mean']
-        value_4 = self.get_design_current_density(cfg)['deterioration_final_current_density']
-        
-        initial_demand = value_2 * value * 0.001
-        mean_demand = value_2 * value_3 * 0.001
-        final_demand = value_2 * value_4 * 0.001
+        return design_current_density
 
+    def get_current_demand(self, cfg, structure_area, current_density):
+        """
+        This method is used to calculate the current demand
+        """
+        
+        coating_breakdown_type = cfg['inputs']['structure']['area']['coating_breakdown']['type']
+
+           
+        coated_area = structure_area['coated']
+        uncoated_area = structure_area['uncoated']
+        
+            
+        initial_demand = coated_area * current_density['initial']['coated'] + uncoated_area * current_density['initial']['uncoated']
+        mean_demand = coated_area * current_density['mean'][coating_breakdown_type]['coated'] + uncoated_area * current_density['mean'][coating_breakdown_type]['uncoated']
+        final_demand = coated_area * current_density['final'][coating_breakdown_type]['coated'] + uncoated_area * current_density['final'][coating_breakdown_type]['uncoated']
+
+        initial_demand = initial_demand/1000
+        mean_demand = mean_demand/1000
+        final_demand = final_demand/1000
+        
         current_demand = {'initial_demand': initial_demand,'mean_demand': mean_demand,'final_demand': final_demand}
+
         return current_demand
 
     
