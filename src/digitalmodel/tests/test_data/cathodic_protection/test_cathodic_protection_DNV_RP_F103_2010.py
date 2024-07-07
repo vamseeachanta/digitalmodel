717fcf0dbbaba7e192ed90d6035ea16fc65734af
Vamsee Achanta <achantav@gmail.com>
2024-07-06 20:59:51 -0500

    20240607

---
+++ src/digitalmodel/tests/test_data/cathodic_protection/test_cathodic_protection_DNV_RP_F103_2010.py
@@ -0,0 +1,47 @@
+import pytest
+import deepdiff
+import os
+import sys
+
+from assetutilities.common.yml_utilities import ymlInput
+
+from digitalmodel.engine import engine
+
+
+def run_cathodic_protection(input_file, expected_result={}):
+    if input_file is not None and not os.path.isfile(input_file):
+        input_file = os.path.join(os.path.dirname(__file__), input_file)
+    cfg = engine(input_file)
+
+    obtained_result = cfg['outputs']['summary'].copy()
+
+    assert not deepdiff.DeepDiff(obtained_result,
+                                 expected_result,
+                                 ignore_order=True,
+                                 significant_digits=4)
+
+def get_valid_pytest_output_file(pytest_output_file):
+    if pytest_output_file is not None and not os.path.isfile(
+            pytest_output_file):
+        pytest_output_file = os.path.join(os.path.dirname(__file__),
+                                          pytest_output_file)
+    return pytest_output_file
+
+
+def test_cathodic_protection():
+    input_file = 'cathodic_protection_DNV_RP_F103_2010.yml'
+    input_file = get_valid_pytest_output_file(input_file)
+
+    pytest_output_file = None
+    # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
+    # expected_result = ymlInput(pytest_output_file, updateYml=None)
+    expected_result = {}
+
+
+    if len(sys.argv) > 1:
+        sys.argv.pop()
+
+    run_cathodic_protection(input_file, expected_result)
+
+
+test_cathodic_protection()
