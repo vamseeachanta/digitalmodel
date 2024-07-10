a7f53c6006d04af93bebf3d2a00d9bead5a6c896
Vamsee Achanta <achantav@gmail.com>
2024-07-10 15:01:19 -0500

    20241007

--- dev_tools/git_all_repos.ps1
+++ dev_tools/git_all_repos.ps1
@@ -3,6 +3,7 @@
 $dir = dir C:\github | ?{$_.PSISContainer}
 
 foreach ($d in $dir){
+    echo $d
     $daily_routine_batch = Join-Path $d.FullName -ChildPath ("\dev_tools\daily_routine.bat")
     
     & $daily_routine_batch
