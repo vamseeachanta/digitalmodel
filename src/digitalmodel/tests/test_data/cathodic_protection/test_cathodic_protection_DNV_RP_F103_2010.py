9990cddce9487430c8caa9365d7a2b0ace4cecaf
Vamsee Achanta <achantav@gmail.com>
2024-07-16 11:24:48 -0500

    add test

--- src/digitalmodel/tests/test_data/cathodic_protection/test_cathodic_protection_DNV_RP_F103_2010.py
+++ src/digitalmodel/tests/test_data/cathodic_protection/test_cathodic_protection_DNV_RP_F103_2010.py
@@ -1,10 +1,11 @@
-import pytest
-import deepdiff
+# Standard library imports
 import os
 import sys
 
-from assetutilities.common.yml_utilities import ymlInput
+# Third party imports
+import deepdiff
 
+# Reader imports
 from digitalmodel.engine import engine
 
 
@@ -13,10 +14,10 @@ def run_cathodic_protection(input_file, expected_result={}):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
     cfg = engine(input_file)
 
-    obtained_result = cfg['outputs']['summary'].copy()
+    obtained_result = cfg['cathodic_protection'].copy()
 
     assert not deepdiff.DeepDiff(obtained_result,
-                                 expected_result,
+                                 expected_result['cathodic_protection'],
                                  ignore_order=True,
                                  significant_digits=4)
 
@@ -32,10 +33,10 @@ def test_cathodic_protection():
     input_file = 'cathodic_protection_DNV_RP_F103_2010.yml'
     input_file = get_valid_pytest_output_file(input_file)
 
-    pytest_output_file = None
-    # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
-    # expected_result = ymlInput(pytest_output_file, updateYml=None)
-    expected_result = {}
+    pytest_output_file = 'cathodic_protection_DNV_RP_F103_2010_pytest.yml'
+    pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
+    expected_result = ymlInput(pytest_output_file, updateYml=None)
+    # expected_result = expected_result['cathodic_protection']
 
 
     if len(sys.argv) > 1:
