dc724c444cf84aac9e15e422e49fdda7bb6e1e7f
Vamsee Achanta <achantav@gmail.com>
2024-07-24 12:02:48 -0500

    20242407

--- src/digitalmodel/tests/test_data/viv_analysis/test_viv_analysis.py
+++ src/digitalmodel/tests/test_data/viv_analysis/test_viv_analysis.py
@@ -10,18 +10,25 @@ def run_process(input_file, expected_result={}):
     if input_file is not None and not os.path.isfile(input_file):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
     cfg = engine(input_file)
-    assert (True)
+
+    obtained_result = cfg['pipeline']['lateral_buckling'].copy()
+    expected_result = expected_result['pipeline']['lateral_buckling'].copy()
+
+    assert not deepdiff.DeepDiff(obtained_result,
+                                 expected_result,
+                                 ignore_order=True,
+                                 significant_digits=4)
 
 
 def test_run_process():
     input_file = 'viv_analysis.yml'
-    pytest_output_file = None
-    # pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
-    # expected_result = ymlInput(pytest_output_file, updateYml=None)
+    pytest_output_file = 'results/pytest_viv_analysis.yml'
+    pytest_output_file = get_valid_pytest_output_file(pytest_output_file)
+    expected_result = ymlInput(pytest_output_file, updateYml=None)
 
     if len(sys.argv) > 1:
         sys.argv.pop()
 
-    run_process(input_file, expected_result={})
+    run_process(input_file, expected_result)
 
 test_run_process()
