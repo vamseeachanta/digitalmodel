7551668652e88527b6dd4b1516e3039d675ba643
Vamsee Achanta <achantav@gmail.com>
2024-11-20 13:52:07 -0600

    daily_routine and git_all_repos recreated for gitbash.

---
+++ dev_tools/bash_tools/daily_routine.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# shell script to perform daily git operations
+repo_root=$(git rev-parse --show-toplevel)
+# get to repo root
+cd "$repo_root"
+
+repo_name=$(basename $(git rev-parse --show-toplevel))
+bash_tools_home="dev_tools/bash_tools"
+today=$(date '+%Y%m%d')
+
+# source common utilities
+source ${bash_tools_home}/common.sh
+
+cat << COM
+Starting daily git routine. key details 
+  - Repository name: $repo_name
+  - Repository root: $repo_root
+  - Commit date: $today
+Executing git operations now 
+COM
+
+# get to repo root
+cd "$repo_root"
+
+# perform git operations
+git pull
+git add --all
+git commit -m "$today"
+git push
+
+log_message "Daily git operations completed"
---
+++ dev_tools/bash_tools/git_all_repos.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+# Directory containing GitHub repositories
+# ensure this path is correct, manually. 
+github_dir="/k/github"
+
+# rel path top bash_tools dir, daily_routine_script
+bash_tools_home="dev_tools/bash_tools"
+daily_routine_script_rel_path="${bash_tools_home}/daily_routine.sh"
+
+# having to hard code this as opposed to sourcing common.sh. 
+# since i have no guarantee on common.sh 
+log_message2() {
+    local color=$1
+    local msg=$2
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    case $color in
+        "red") echo -e "\033[31m$msg\033[0m" ;;
+        "green") echo -e "\033[32m$msg\033[0m" ;;
+        "yellow") echo -e "\033[33m$msg\033[0m" ;;
+    esac
+    echo "[${timestamp}] ${msg}" # >> "${LOG_FILE}"
+}
+
+log_message "Starting repository check process..."
+
+# Iterate through all directories in the GitHub folder
+for dir in "$github_dir"/*/ ; do
+    if [ -d "$dir" ]; then
+        log_message2 "green" "Checking for changes in folder: $dir"
+        cd "$dir"
+
+        # Check if there are any changes
+        if [ -n "$(git status --porcelain)" ]; then
+            log_message2 "yellow" "Changes detected in $(basename "$dir")"
+            
+            # Execute daily routine script if it exists
+            # daily_routine_script="${dir}dev_tools/daily_routine.sh"
+            daily_routine_script="${dir}/${daily_routine_script_rel_path}"
+            if [ -f "$daily_routine_script" ]; then
+                log_message2 "green" "Running daily routine script..."
+                bash "$daily_routine_script"
+                log_message2 "green" "Daily routine completed"
+            else
+                log_message2 "red" "Daily routine script not found: $daily_routine_script"
+            fi
+        else
+            log_message2 "green" "No changes detected in $(basename "$dir")"
+        fi
+    fi
+done
+
+# Return to original directory
+cd "$script_dir"
+log_message2 "green" "Completed daily_routine for all repositories"
