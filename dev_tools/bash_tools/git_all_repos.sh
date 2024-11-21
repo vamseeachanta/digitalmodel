e7dd5f49be6b5b69ac5e81737c458974037c9b25
Sivakumar Pabolu <sivakumar.pabolu>
2024-11-21 11:38:22 -0600

    more minor tweaks.

--- dev_tools/bash_tools/git_all_repos.sh
+++ dev_tools/bash_tools/git_all_repos.sh
@@ -29,13 +29,14 @@ log_message() {
     echo "[${timestamp}] ${msg}" # >> "${LOG_FILE}"
 }
 
-log_message "normal" "Starting repository check process..."
+cd ${github_dir}
+log_message "normal" "Starting repository check-in routine process in $(pwd)..."
 
 # Iterate through all directories in the GitHub folder
 for dir in "$github_dir"/*/ ; do
     if [ -d "$dir" ]; then
-        # - [ ] todo - make these checking white 
-        log_message "green" "Checking for changes in folder: $dir"
+
+        log_message "normal" "Checking for changes in folder: $dir"
         cd "$dir"
 
         # Check if there are any changes
@@ -46,18 +47,18 @@ for dir in "$github_dir"/*/ ; do
             # daily_routine_script="${dir}dev_tools/daily_routine.sh"
             daily_routine_script="${dir}/${daily_routine_script_rel_path}"
             if [ -f "$daily_routine_script" ]; then
-                log_message "green" "Running daily routine script..."
+                log_message "green" "Running daily routine script in $(basename "$dir") ..."
                 bash "$daily_routine_script"
-                log_message "green" "Daily routine completed"
+                log_message "green" "Daily routine completed in $(basename "$dir") ..."
             else
                 log_message "red" "Daily routine script not found: $daily_routine_script"
             fi
         else
-            log_message "green" "No changes detected in $(basename "$dir")"
+            log_message "green" "No changes detected in $(basename "$dir") ..."
         fi
     fi
 done
 
 # Return to original directory
-cd "$script_dir"
+cd "$github_dir"
 log_message "green" "Completed daily_routine for all repositories"
