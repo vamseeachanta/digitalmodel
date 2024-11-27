2526d247891794ec0f41e3ed94d6fc4bfc925d9e
Vamsee Achanta <achantav@gmail.com>
2024-11-27 10:44:25 -0600

    20242711

--- tests/modules/orcaflex/orcaflex_post_process/test_orcaflex.py
+++ tests/modules/orcaflex/orcaflex_post_process/test_orcaflex.py
@@ -1,15 +1,16 @@
 # Third party imports
-from test_opp_summary1 import test_process as test_opp_summary1_test
-from test_opp_summary2_master import test_process as test_opp_summary2_master_test
-from test_opp_time_series1 import test_process as test_opp_time_series1_test
+from test_opp_summary1 import test_process as test_opp_summary1_copy
+from test_opp_summary2_master import test_process as test_opp_summary2_master_copy
+from test_opp_time_series1 import test_process as test_opp_time_series1_copy
 from test_opp_time_series2_master import (
-    test_process as test_opp_time_series2_master_test,
+    test_process as test_opp_time_series2_master_copy,
 )
 from test_orcaflex_license import test_orcaflex_license
+
 test_orcaflex_license()
 
-test_opp_summary1_test()
-test_opp_summary2_master_test()
+test_opp_summary1_copy()
+test_opp_summary2_master_copy()
 
-test_opp_time_series1_test()
-test_opp_time_series2_master_test()
+test_opp_time_series1_copy()
+test_opp_time_series2_master_copy()
