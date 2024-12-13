4888f0e6f1764966e3ffa8921be4d07ded3784ae
Vamsee Achanta <achantav@gmail.com>
2024-12-13 13:31:12 -0600

    20241312

---
+++ docs/pkg_autocad/adding_apps.md
@@ -0,0 +1,4 @@
+
+- Using APPLOAD command load the file app file code eg: "make3dpoly.txt"
+- Type MAKE3DPOLYFROMCSV at the autocad command prompt and press ENTER
+- This should create the polyline using the coordinates created by .csv file.
--- docs/pkg_qgis/project1/py/load_lakach_lines.py
+++ docs/pkg_qgis/project1/py/load_lakach_lines.py
@@ -21,27 +21,27 @@ def load_dxf_layer(dxf_filename, layer_name):
 
 # Input Files
 
-# layer_name = "18inch_flowline_main"
-# dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\18inch Flowline_4.dxf"
-# load_dxf_layer(dxf_filename, layer_name)
+layer_name = "18inch_flowline_main"
+dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\18inch Flowline_4.dxf"
+load_dxf_layer(dxf_filename, layer_name)
 
-# layer_name = "18inch_flowline_infield"
-# dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\18inch Flowline_4.1.dxf"
-# load_dxf_layer(dxf_filename, layer_name)
+layer_name = "18inch_flowline_infield"
+dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\18inch Flowline_4.1.dxf"
+load_dxf_layer(dxf_filename, layer_name)
 
-# layer_name = "umbilical_main"
-# dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\Main Umbilical_2.1.dxf"
-# load_dxf_layer(dxf_filename, layer_name)
+layer_name = "umbilical_main"
+dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\Main Umbilical_2.1.dxf"
+load_dxf_layer(dxf_filename, layer_name)
 
-# layer_name = "umbilical_infield"
-# dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\Main Infield Umbilical_2.1.dxf"
-# load_dxf_layer(dxf_filename, layer_name)
+layer_name = "umbilical_infield"
+dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\Main Infield Umbilical_2.1.dxf"
+load_dxf_layer(dxf_filename, layer_name)
 
 layer_name = "4inch_ct"
 dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\4inch Coiled Pipeline_2.1.dxf"
 load_dxf_layer(dxf_filename, layer_name)
 
-# layer_name = "4inch_saline"
-# dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\4inch Saline Diffuser Line_2.1.dxf"
-# load_dxf_layer(dxf_filename, layer_name)
+layer_name = "4inch_saline"
+dxf_filename = r"\\dgi-hou-fs01\Projects\61863 Talos Lakach FEED\999 Work Space\VAchanta\qgis\inputs\lines\4inch Saline Diffuser Line_2.1.dxf"
+load_dxf_layer(dxf_filename, layer_name)
 
