847a6674a0008262de2d0075f9ddf8677f9f1c54
Vamsee Achanta <achantav@gmail.com>
2025-05-31 03:39:41 -0500

    wip

--- docs/pkg_orcaflex/scripts/thread_count.sh
+++ docs/pkg_orcaflex/scripts/thread_count.sh
@@ -1,2 +1,13 @@
+For 12 cores:
+<code>
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Orcina\OrcaFlex /v DefaultThreadCount /t REG_DWORD /d 12 /f
+</code>
+
+For 60 cores:
+<code>
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Orcina\OrcaFlex /v DefaultThreadCount /t REG_DWORD /d 60 /f
+</code>
+
+or in UI:
 [HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Orcina\OrcaFlex]
 "DefaultThreadCount"=dword:0000000c
